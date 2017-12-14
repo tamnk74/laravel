05ac4c1661192c7ee7c089a8310e6f1fd2514f9d
Khac Tam Nguyen <khac.tam.94@gmail.com>
2017-12-14 22:55:47 +0700

    setup front-end

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var elixir = require('laravel-elixir');
+
+/**
+ * Copy any needed files.
+ *
+ * Do a 'gulp copyfiles' after bower updates
+ */
+gulp.task("copyfiles", function() {
+
+  gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
+    .pipe(gulp.dest("resources/assets/js/"));
+
+  gulp.src("vendor/bower_dl/bootstrap/less/**")
+    .pipe(gulp.dest("resources/assets/less/bootstrap"));
+
+  gulp.src("vendor/bower_dl/bootstrap/dist/js/bootstrap.js")
+    .pipe(gulp.dest("resources/assets/js/"));
+
+  gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
+    .pipe(gulp.dest("public/assets/fonts"));
+
+});
+
+/**
+ * Default gulp is to run this elixir stuff
+ */
+elixir(function(mix) {
+
+  // Combine scripts
+  mix.scripts([
+      'js/jquery.js',
+      'js/bootstrap.js'
+    ],
+    'public/assets/js/admin.js',
+    'resources/assets'
+  );
+
+  // Compile Less
+  mix.less('admin.less', 'public/assets/css/admin.css');
+});
